8c6ac4ec8e7626a1a7b4212b0c57ff2df9658a1a
Anton <kokhanchik.tversu@gmail.com>
2016-10-20 22:16:09 +0300

    table logic fixes

--- task.sql
+++ task.sql
@@ -3,19 +3,19 @@ use university;
 
 create table cities
 (
-    city_id int(20) auto_increment,
+    city_id smallint auto_increment,
     city_name varchar(15) not null,
     city_country varchar(15) not null,
     primary key(city_id)
 );
 create table students
 (
-    student_id int(20) auto_increment,
+    student_id smallint auto_increment,
     name varchar(15) not null,
-    surname varchar(15),
-    course tinyint(5),
+    surname varchar(15) not null,
+    course tinyint(1) not null,
     gener enum('male','female') not null,
-    city_id int(20),
+    city_id smallint not null,
     primary key(student_id),
     foreign key(city_id) references cities(city_id)
 );
